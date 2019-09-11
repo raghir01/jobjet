ca437619346f71ea087e66416763701254e3ee90
Raghi Ramachandran <raghir01@gmail.com>
2019-09-10 22:19:27 -0400

    Added all the CRUD methods in service layer

--- jobjet/src/main/java/com/company/jobjet/service/JobService.java
+++ jobjet/src/main/java/com/company/jobjet/service/JobService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class JobService {
     JobDao jobDao;
@@ -26,5 +29,48 @@ public class JobService {
         job = jobDao.addJob(job);
         jobViewModel.setJobId(job.getJobId());
         return jobViewModel;
+    }
+
+    public JobViewModel getJob(int jobId){
+        Job job = jobDao.getJob(jobId);
+        JobViewModel jobViewModel = new JobViewModel();
+        jobViewModel.setJobId(job.getJobId());
+        jobViewModel.setJobDescription(job.getJobDescription());
+        jobViewModel.setPostedDate(job.getPostedDate());
+        jobViewModel.setStatus(job.getStatus());
+
+        return jobViewModel;
+    }
+
+    public List<JobViewModel> getAllJobs(){
+        List<Job> jobList = jobDao.getAllJobs();
+        List<JobViewModel> jobViewModelList = new ArrayList<>();
+        for(Job job : jobList){
+            JobViewModel jobViewModel = new JobViewModel();
+            jobViewModel.setJobId(job.getJobId());
+            jobViewModel.setJobTitle(job.getJobTitle());
+            jobViewModel.setJobDescription(job.getJobDescription());
+            jobViewModel.setPostedDate(job.getPostedDate());
+            jobViewModel.setStatus(job.getStatus());
+
+            jobViewModelList.add(jobViewModel);
+        }
+        return jobViewModelList;
+    }
+
+
+    public JobViewModel updateJob(JobViewModel jobViewModel){
+        Job job = new Job();
+        job.setJobTitle(jobViewModel.getJobTitle());
+        job.setJobDescription(jobViewModel.getJobDescription());
+        job.setPostedDate(jobViewModel.getPostedDate());
+        job.setStatus(jobViewModel.getStatus());
+        job.setJobId(jobViewModel.getJobId());
+        jobDao.updateJob(job);
+        return jobViewModel;
+    }
+
+    public void deleteJob(int jobId){
+        jobDao.deleteJob(jobId);
     }
 }
---
+++ jobjet/src/test/java/com/company/jobjet/service/JobServiceTest.java
@@ -0,0 +1,28 @@
+package com.company.jobjet.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JobServiceTest {
+
+    @Test
+    public void addJob() {
+    }
+
+    @Test
+    public void getJob() {
+    }
+
+    @Test
+    public void getAllJobs() {
+    }
+
+    @Test
+    public void updateJob() {
+    }
+
+    @Test
+    public void deleteJob() {
+    }
+}

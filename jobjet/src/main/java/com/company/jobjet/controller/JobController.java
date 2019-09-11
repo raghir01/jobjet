cb1c937f579a642f08d0310dac3c70a4ea5d6529
Raghi Ramachandran <raghir01@gmail.com>
2019-09-11 18:03:55 -0400

    Controller added

--- jobjet/src/main/java/com/company/jobjet/controller/JobController.java
+++ jobjet/src/main/java/com/company/jobjet/controller/JobController.java
@@ -2,22 +2,44 @@ package com.company.jobjet.controller;
 
 import com.company.jobjet.dao.JobDao;
 import com.company.jobjet.dto.Job;
+import com.company.jobjet.service.JobService;
+import com.company.jobjet.viewmodel.JobViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/jobs")
 public class JobController {
 
     @Autowired
-    private JobDao jobDao;
+    private JobService jobService;
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    Job addJob(@RequestBody @Valid Job job){
-        return jobDao.addJob(job);
+    JobViewModel addJob(@RequestBody @Valid JobViewModel jobViewModel){
+        return jobService.addJob(jobViewModel);
     }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    JobViewModel getJob(@PathVariable int id){
+        return jobService.getJob(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    List<JobViewModel> getAllJobs(){
+        return jobService.getAllJobs();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    void deleteJob(@PathVariable int id){
+         jobService.deleteJob(id);
+    }
+
 }
